cf9a1b99a6c52f28c73799bb04f915d5cc639a17
Robert Rogers <rerogers@gmail.com>
2023-03-26 22:34:47 -0500

    updated exercise 2 for LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var games = new List<string> {"Super Mario Brothers", "Uncharted", "Uncharted 2", "Uncharted 3", "MLB The Show 2019", 
+                "MLB The Show 2023", "TECMO Super Bowl", "Sudoku", "Lego Lord of the Rings" };
+            
+            var NewGames = games.OrderBy(x => x.Length);
+            foreach (var game in NewGames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
